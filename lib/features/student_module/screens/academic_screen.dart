72d2f61639c1bcf0fb16321a2af371fadd907f22
root <root@vmi2949909.contaboserver.net>
2026-01-07 07:24:32 +0100

    fix(academic): add missing import for DataService

--- lib/features/student_module/screens/academic_screen.dart
+++ lib/features/student_module/screens/academic_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../../core/theme/app_theme.dart';
+import '../../../../core/services/data_service.dart';
 
 class AcademicScreen extends StatefulWidget {
   const AcademicScreen({super.key});
